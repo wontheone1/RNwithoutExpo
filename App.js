8d42c0435dde287077a29b04e3abd66b0fda7b83
Won Seob Seo <won.seob.seo@zalando.fi>
2020-04-10 14:27:01 +0300

    Add onKeyDownListener

--- App.js
+++ App.js
@@ -7,33 +7,40 @@
  */
 
 import React from 'react';
+import KeyEvent from 'react-native-keyevent';
+
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
+  TextInput,
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+const App = () => {
+  React.useEffect(() => {
+    console.log('KeyEvent');
+    console.log(KeyEvent);
+    console.log(KeyEvent.onKeyDownListener);
+    console.log(KeyEvent.onKeyUpListener);
+    console.log(KeyEvent.onKeyMultipleListener);
+    console.log(KeyEvent.thereShouldBeNothing);
+    KeyEvent.onKeyDownListener((keyEvent) => {
+      console.log('inside listener');
+      console.log(`onKeyDown keyCode: ${keyEvent.keyCode}`);
+      console.log(`Action: ${keyEvent.action}`);
+      console.log(`Key: ${keyEvent.pressedKey}`);
+    });
+  });
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView>
           <View>
-            <View>
-              <Text>Hello world!</Text>
-            </View>
+            <Text>Hello world!</Text>
           </View>
         </ScrollView>
       </SafeAreaView>
--- android/app/src/main/java/com/rnwithoutexpo/MainActivity.java
+++ android/app/src/main/java/com/rnwithoutexpo/MainActivity.java
@@ -1,15 +1,48 @@
 package com.rnwithoutexpo;
 
+import android.view.KeyEvent;
 import com.facebook.react.ReactActivity;
+import com.github.kevinejohn.keyevent.KeyEventModule;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
     return "RNwithoutExpo";
+  }
+
+  @Override // <--- Add this method if you want to react to keyDown
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+    // A. Prevent multiple events on long button press
+    // In the default behavior multiple events are fired if a button
+    // is pressed for a while. You can prevent this behavior if you
+    // forward only the first event:
+    // if (event.getRepeatCount() == 0) {
+    // KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
+    // }
+    //
+    // B. If multiple Events shall be fired when the button is pressed
+    // for a while use this code:
+    // KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
+    //
+    // Using B.
+    KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
+
+    // There are 2 ways this can be done:
+    // 1. Override the default keyboard event behavior
+    // super.onKeyDown(keyCode, event);
+    // return true;
+
+    // 2. Keep default keyboard event behavior
+    // return super.onKeyDown(keyCode, event);
+
+    // Using method #1 without blocking multiple
+    super.onKeyDown(keyCode, event);
+    return true;
   }
 }
